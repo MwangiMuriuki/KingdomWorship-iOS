076a6fd7fd45c66adb9f016b15d7ab4d5b8dc1d2
Ernest Muriuki <ernest.muriuki@squaddigital.com>
2021-09-10 19:15:21 +0300

    first commit

---
+++ Ksf Worship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ksf Worship
+//
+//  Created by ERNEST MURIUKI on 10/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Ksf Worship/Pods-Ksf Worship-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ksf_Worship : NSObject
+@end
+@implementation PodsDummy_Pods_Ksf_Worship
+@end
